7aecc7bfff286376dd6c0e953cf7daaf42686e0c
kizzyhub <bellokazeem908@gmail.com>
2025-04-25 02:08:54 +0100

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Logo from "../assets/Logo.svg";
+import { BsTwitter } from "react-icons/bs";
+import { SiLinkedin } from "react-icons/si";
+import { BsYoutube } from "react-icons/bs";
+import { FaFacebookF } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-section-one">
+        <div className="footer-logo-container">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="footer-icons">
+          <BsTwitter />
+          <SiLinkedin />
+          <BsYoutube />
+          <FaFacebookF />
+        </div>
+      </div>
+      <div className="footer-section-two">
+        <div className="footer-section-columns">
+          <span>Company</span>
+          <span>Help</span>
+          <span>Share</span>
+          <span>carrers</span>
+          <span>Testimonials</span>
+          <span>Work</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>+2348103232868</span>
+          <span>bellokazeem908@gmail.com</span>
+          <span>bellokazeem54@gmail.com</span>
+          <span>contact@food.com</span>
+        </div>
+        <div className="footer-section-columns">
+          <span>Terms & Conditions</span>
+          <span>Privacy Policy</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
